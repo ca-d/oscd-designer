faed855c3d0289421093400c0c80a9aaa53088da
Christian Dinkel <christian.dinkel@omicronenergy.com>
2023-12-01 12:08:53 +0100

    wip: fix more bugs

--- sld-editor.ts
+++ sld-editor.ts
@@ -414,7 +414,8 @@ export class SLDEditor extends LitElement {
     }
 
     const containingParent =
-      element.tagName === 'VoltageLevel'
+      element.tagName === 'VoltageLevel' ||
+      element.tagName === 'PowerTransformer'
         ? containsRect(this.substation, x, y, w, h)
         : Array.from(
             this.substation.querySelectorAll(
@@ -630,6 +631,7 @@ export class SLDEditor extends LitElement {
             'TapChanger'
           );
           node.setAttribute('name', 'LTC');
+          node.setAttribute('type', 'LTC');
           node.setAttribute('name', uniqueName(node, winding));
           this.dispatchEvent(
             newEditEvent({
@@ -2016,11 +2018,19 @@ export class SLDEditor extends LitElement {
                 y,
               })
             );
-          } else {
-            let placing = transformer;
-            if (e.shiftKey) placing = copy(transformer, this.nsp);
-            this.dispatchEvent(newStartPlaceEvent(placing));
           }
+
+          if (
+            this.placing ||
+            this.connecting ||
+            this.resizing ||
+            this.placingLabel
+          )
+            return;
+
+          let placing = transformer;
+          if (e.shiftKey) placing = copy(transformer, this.nsp);
+          this.dispatchEvent(newStartPlaceEvent(placing));
         }}>
         ${windings.map(w => this.renderTransformerWinding(w))}
       </g>
