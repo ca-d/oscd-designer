396a365999af0bef187676e880e9f18dba0f93b7
Christian Dinkel <christian.dinkel@omicronenergy.com>
2023-12-05 16:03:50 +0100

    Updates

--- sld-editor.js
+++ sld-editor.js
@@ -325,9 +325,11 @@ let SLDEditor = class SLDEditor extends LitElement {
             y += this.mouseY - parentY;
         }
         if (this.resizingTL === element) {
-            const { pos: [resX, resY], dim: [resW, resH], } = attributes(this.resizingTL);
-            x += Math.min(this.mouseX, resX + resW - 1) - resX;
-            y += Math.min(this.mouseY, resY + resH - 1) - resY;
+            const { pos: [resX, resY], dim: [resW, resH], } = attributes(element);
+            if (resX === x && resY === y) {
+                x += Math.min(this.mouseX, resX + resW - 1) - resX;
+                y += Math.min(this.mouseY, resY + resH - 1) - resY;
+            }
         }
         if (this.placingLabel === element) {
             x = this.mouseX2 - 0.5;
