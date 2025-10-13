53a189052cbabcf59d9ae9a785ccebe94d29111c
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-10-13 10:35:20 +0200

    chore: update deps, added missing deps, updated rollup + web-dev-server configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import copy from 'rollup-plugin-copy';
 import fs from 'fs';
 
 const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'utf8'));
@@ -17,7 +18,7 @@ export default [
       sourcemap: true, // Add source map to build output
       format: 'es', // ES module type export
       dir: 'dist', // The build output folder
-      // preserveModules: true,  // Keep directory structure and files
+      // preserveModules: true, // Keep directory structure and files
     },
     preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
 
@@ -38,10 +39,22 @@ export default [
         input: 'demo/index.html',
         minify: true,
       }),
+      /** Resolve bare module imports */
       nodeResolve(),
+
       typescript(demoTsconfig),
+
       /** Bundle assets references via import.meta.url */
       importMetaAssets(),
+      copy({
+        targets: [
+          { src: 'demo/sample.scd', dest: 'dist/demo' },
+          { src: 'demo/*.js', dest: 'dist/demo' },
+          // Add more patterns if you have more assets
+        ],
+        verbose: true,
+        flatten: false,
+      }),
     ],
     output: {
       dir: 'dist/demo',
--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,29 +1,22 @@
-// import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
 
-/** Use Hot Module replacement by adding --hmr to the start command */
 // eslint-disable-next-line no-undef
 const hmr = process.argv.includes('--hmr');
 
+//Details & options: https://modern-web.dev/docs/dev-server/overview/
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   rootDir: 'dist',
   open: 'demo/index.html',
   /** Use regular watch mode if HMR is not enabled. */
   watch: !hmr,
-  /** Resolve bare module imports */
-  // nodeResolve: {
-  //   exportConditions: ['browser', 'development'],
-  // },
-
-  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto'
-
-  /** Set appIndex to enable SPA routing */
-  // appIndex: '/demo/index.html',
 
   plugins: [
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
-    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.lit] }),
+    hmr &&
+      //Details & options: https://open-wc.org/docs/development/hot-module-replacement/
+      hmrPlugin({
+        exclude: ['**/*/node_modules/**/*'],
+        presets: [presets.lit],
+      }),
   ],
-
-  // See documentation for all available options
 });
